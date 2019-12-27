331e4fc39bcc5bcc024d2281a0a7d76f3d1c1c6f
Robert Shields <robert.shieldsiv@gmail.com>
2019-12-27 03:35:04 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -10,7 +10,7 @@ class Anagram
   
   def match(array)
     array.select do 
-      |x| x.split("").sort == @name.split("").sort
+      |x| x.split("").sort == @word.split("").sort
     end
   end   
     
